707561991733132e648559163e89a325680d5ab5
Felie Bento <felipe.16costa@gmail.com>
2021-04-27 14:41:25 -0300

    HttpHandler

--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -4,39 +4,33 @@
     import Category from "./category.svelte";
     import Product from "../routes/product.svelte";
     import NavLink from "../components/nav-link.svelte";
+    import HttpHandler from "../http/http";
+    import { onMount } from "svelte";
+
+    let httpHandler = new HttpHandler();
+    let canSee = false;
+
+    onMount(async () => {
+        
+        const res = await httpHandler.getInfo("info").then((result) => result);
+        let isAdmin = res.authorities.filter(item => item.authority === "ROLE_ADMIN");
+        
+        if (isAdmin.length > 0) {
+            canSee = true;
+        }
+
+    });
 </script>
-
-<style>
-    ul {
-        padding: 50px;
-        list-style: none;
-        background: #3f3f3f;
-        height: 100vh;
-    }
-
-    li {
-        margin: 20px;
-        cursor: pointer;
-        color: #ffffff;
-        border: 1px solid black;
-        padding: 15px;
-        background-color: white;
-        text-align: center;
-        border-radius: 10px;
-    }
-
-    .components {
-        padding: 50px;
-    }
-</style>
 
 <Router>
     <div class="row">
         <div class="col-3">
             <ul>
-                <li>
-                    <NavLink to="user">Users</NavLink>
-                </li>
+                {#if canSee}
+                    <li>
+                        <NavLink to="user">Users</NavLink>
+                    </li>
+                {/if}
 
                 <li>
                     <NavLink to="category">Categories</NavLink>
@@ -68,3 +62,27 @@
         </div>
     </div>
 </Router>
+
+<style>
+    ul {
+        padding: 50px;
+        list-style: none;
+        background: #3f3f3f;
+        height: 100vh;
+    }
+
+    li {
+        margin: 20px;
+        cursor: pointer;
+        color: #ffffff;
+        border: 1px solid black;
+        padding: 15px;
+        background-color: white;
+        text-align: center;
+        border-radius: 10px;
+    }
+
+    .components {
+        padding: 50px;
+    }
+</style>
